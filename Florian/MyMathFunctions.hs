1feec65089bc72a714452d3cd6513e05ba48d8d1
Jan Achilles <jan.achilles@ostfalia.de>
2021-11-18 14:45:47 +0100

    bearbeitet

--- Florian/MyMathFunctions.hs
+++ Florian/MyMathFunctions.hs
@@ -27,25 +27,3 @@ lean a = if a > 9 then 1 + lean (div  a 10)
 
 bekommeZufallPrime :: (Random a, RandomGen g, Integral b, Num a) => g -> b -> (a, g)
 bekommeZufallPrime seed  laenge= langeZufallszahl seed laenge
-
-
-langeZufallszahl seed laenge | laenge < 2 =  randomR (0,1) seed
-                            | otherwise = randomR (2^(laenge-1),2^laenge-1) seed
-
-
-laengeAnpassen zahl =  if zahl < 100 then ('0':show zahl) else show(zahl)
-
-umwandelnInBuchstabe zahl = toEnum zahl
-
-umwandelnInZahl ch = fromEnum ch
-
-
-woerterAneinanderKlatschen [] = ""
-woerterAneinanderKlatschen (satz) =  laengeAnpassen (umwandelnInZahl ( head satz)) ++ (woerterAneinanderKlatschen (tail satz))
-
-nimmDrei satz =  read satz  :: Int
-
-zAA a = zahlenAneinanderKlatschen a 1
-zahlenAneinanderKlatschen []  c = 0
-zahlenAneinanderKlatschen a c = fromEnum (last  a) * c + zahlenAneinanderKlatschen (init a)  (c*1000)
-
