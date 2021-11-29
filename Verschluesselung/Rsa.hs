5091e4416b7f08446521bb7b1885c9dd16301a32
Jan Achilles <jan.achilles@ostfalia.de>
2021-11-29 22:12:21 +0100

    executable

---
+++ Verschluesselung/Rsa.hs
@@ -0,0 +1,37 @@
+module Rsa
+(
+mainRsa
+)where
+import Generate
+import Verschluesselung
+import Entschluesselung
+
+mainRsa = do
+    putStrLn "Möchtest du:"
+    putStrLn "1. Primzahlengenerieren"
+    putStrLn "2. Verschlüsseln"
+    putStrLn "3. Entschlüsseln"
+    putStrLn "4. Beenden"
+    putStrLn "Eingabe bitte mittels 1, 2, 3 oder 4"
+    putStr ">>"
+
+    decide <- getLine
+
+    if decide == "1"
+    then do
+        generateDo
+    else 
+        if decide == "2"
+        then do
+            verschlMain
+        else
+            if decide == "3"
+            then 
+                entschl
+            else do
+                mainRsa
+    if decide == "4"
+    then putStrLn "Ende"
+    else do 
+        mainRsa
+    --putStrLn "Ende"
